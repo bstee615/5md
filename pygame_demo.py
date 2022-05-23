e63fb2781eb6b953ef52bec0dd16c71e19c1d326
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-05-23 15:18:13 -0500

    Make Handle class

--- pygame_demo.py
+++ pygame_demo.py
@@ -8,41 +8,56 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
+class Handle:
+    def __init__(self, obj):
+        self.obj = obj
+        self.grabbed = False
+        self.grab_offset = pygame.Vector2()
+
+    def __repr__(self):
+        return repr(self.obj)
+
+
 ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+ballrect = Handle(ball.get_rect())
 
-mousex = 10
-mousey = 10
+class GameState:
+    def __init__(self):
+        self.mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
 
-offset = (0, 0)
-grabbed = False
-pressed_left = False
+    def try_pickup(self, ballrect):
+        if ballrect.obj.collidepoint(self.mouse_pos):
+            ballrect.grab_offset = pygame.Vector2(self.mouse_pos.x - ballrect.obj.x, self.mouse_pos.y - ballrect.obj.y)
+            print("mouse", self.mouse_pos, "ball", ballrect, "offset", ballrect.grab_offset)
+            ballrect.grabbed = True
+        else:
+            ballrect.grabbed = False
 
+    def update_mouse_pos(self):
+        self.mouse_pos.update(pygame.mouse.get_pos())
+        
+    def step(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                left, middle, right = pygame.mouse.get_pressed()
+                if left:
+                    self.try_pickup(ballrect)
+            if event.type == pygame.MOUSEBUTTONUP:
+                left, middle, right = pygame.mouse.get_pressed()
+                if not left:
+                    ballrect.grabbed = False
+
+        state.update_mouse_pos()
+
+        if ballrect.grabbed:
+            ballrect.obj.x = self.mouse_pos.x - ballrect.grab_offset[0]
+            ballrect.obj.y = self.mouse_pos.y - ballrect.grab_offset[1]
+
+        screen.fill(black)
+        screen.blit(ball, ballrect.obj)
+        pygame.display.flip()
+
+state = GameState()
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            left, middle, right = pygame.mouse.get_pressed()
-            if left:
-                pressed_left = True
-                if ballrect.collidepoint((mousex, mousey)):
-                    offset = (mousex - ballrect.x, mousey - ballrect.y)
-                    print("mouse", (mousex, mousey), "ball", (ballrect.x, ballrect.y), "offset", offset)
-                    grabbed = True
-                else:
-                    grabbed = False
-        if event.type == pygame.MOUSEBUTTONUP:
-            left, middle, right = pygame.mouse.get_pressed()
-            if not left:
-                pressed_left = False
-
-    mousex, mousey = pygame.mouse.get_pos()
-
-    new_pressed_left = pygame.mouse.get_pressed()[0]
-    if pressed_left and grabbed:
-        ballrect.x = mousex - offset[0]
-        ballrect.y = mousey - offset[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+    state.step()
