4a3aba6865a4565f0a973c20ddb0eac200f413af
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-05-23 15:39:26 -0500

    Revert "Allow multiple grabbed objects"
    This reverts commit b093b33f92139b1366d5afb0484f233fc53658f2.

--- pygame_demo.py
+++ pygame_demo.py
@@ -9,36 +9,29 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 
 class Handle:
-    """Grabbable handle on object"""
-    def __init__(self):
+    def __init__(self, obj):
+        self.obj = obj
         self.grabbed = False
         self.grab_offset = pygame.Vector2()
 
-class MyObject:
-    def __init__(self, obj):
-        self.fields = {}
-        self.fields["object"] = obj
-        self.fields["rect"] = obj.get_rect()
-        self.fields["handle"] = Handle()
+    def __repr__(self):
+        return repr(self.obj)
+
+
+ball = pygame.image.load("intro_ball.gif")
+ballrect = Handle(ball.get_rect())
 
 class GameState:
-    """Global game state"""
     def __init__(self):
         self.mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
-        self.object_handles = []
-        self.grabbed = []
 
-    def add_object(self, obj):
-        self.object_handles.append(MyObject(obj))
-
-    def try_pickup(self):
-        for obj in self.object_handles:
-            obj_rect = obj.fields["rect"]
-            obj_handle = obj.fields["handle"]
-            if obj_rect.collidepoint(self.mouse_pos):
-                obj_handle.grab_offset = pygame.Vector2(self.mouse_pos.x - obj_rect.x, self.mouse_pos.y - obj_rect.y)
-                print("mouse", self.mouse_pos, "ball", obj_rect, "offset", obj_handle.grab_offset)
-                self.grabbed.append(obj)
+    def try_pickup(self, ballrect):
+        if ballrect.obj.collidepoint(self.mouse_pos):
+            ballrect.grab_offset = pygame.Vector2(self.mouse_pos.x - ballrect.obj.x, self.mouse_pos.y - ballrect.obj.y)
+            print("mouse", self.mouse_pos, "ball", ballrect, "offset", ballrect.grab_offset)
+            ballrect.grabbed = True
+        else:
+            ballrect.grabbed = False
 
     def update_mouse_pos(self):
         self.mouse_pos.update(pygame.mouse.get_pos())
@@ -49,26 +42,22 @@ class GameState:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 left, middle, right = pygame.mouse.get_pressed()
                 if left:
-                    self.try_pickup()
+                    self.try_pickup(ballrect)
             if event.type == pygame.MOUSEBUTTONUP:
                 left, middle, right = pygame.mouse.get_pressed()
                 if not left:
-                    self.grabbed = []
+                    ballrect.grabbed = False
 
         state.update_mouse_pos()
-        
-        for o in self.grabbed:
-            o_rect = o.fields["rect"]
-            o_handle = o.fields["handle"]
-            o_rect.x = self.mouse_pos.x - o_handle.grab_offset[0]
-            o_rect.y = self.mouse_pos.y - o_handle.grab_offset[1]
+
+        if ballrect.grabbed:
+            ballrect.obj.x = self.mouse_pos.x - ballrect.grab_offset[0]
+            ballrect.obj.y = self.mouse_pos.y - ballrect.grab_offset[1]
 
         screen.fill(black)
-        for o in self.object_handles:
-            screen.blit(o.fields["object"], o.fields["rect"])
+        screen.blit(ball, ballrect.obj)
         pygame.display.flip()
 
 state = GameState()
-state.add_object(pygame.image.load("intro_ball.gif"))
 while 1:
     state.step()
