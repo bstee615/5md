c477912fdc4695bded41e9d53f85831f1ade8e2d
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-05-23 15:55:06 -0500

    Add more objects and make some fields optional

--- pygame_demo.py
+++ pygame_demo.py
@@ -2,8 +2,7 @@ import sys
 import pygame
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
+size = width, height = 1024, 768
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -15,26 +14,36 @@ class Handle:
         self.grab_offset = pygame.Vector2()
 
 class MyObject:
-    def __init__(self, obj):
+    def __init__(self, obj, rect=True, draggable=False):
         self.fields = {}
         self.fields["object"] = obj
-        self.fields["rect"] = obj.get_rect()
-        self.fields["handle"] = Handle()
+        if rect:
+            self.fields["rect"] = obj.get_rect()
+        if draggable:
+            self.fields["handle"] = Handle()
 
 class GameState:
     """Global game state"""
     def __init__(self):
         self.mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
         self.object_handles = []
+        self.named_objects = {}
 
-    def add_object(self, obj):
-        self.object_handles.append(MyObject(obj))
+    def add_object(self, obj, name=None, **kwargs):
+        my_obj = MyObject(obj, **kwargs)
+        if name is not None:
+            self.named_objects[name] = my_obj
+        obj_id = len(self.object_handles)
+        self.object_handles.append(my_obj)
+        return obj_id
 
     def try_pickup(self):
         for obj in self.object_handles:
+            if not("handle" in obj.fields and "rect" in obj.fields):
+                continue
             obj_rect = obj.fields["rect"]
-            obj_handle = obj.fields["handle"]
             if obj_rect.collidepoint(self.mouse_pos):
+                obj_handle = obj.fields["handle"]
                 obj_handle.grab_offset = pygame.Vector2(self.mouse_pos.x - obj_rect.x, self.mouse_pos.y - obj_rect.y)
                 print("mouse", self.mouse_pos, "ball", obj_rect, "offset", obj_handle.grab_offset)
                 obj_handle.grabbed = True
@@ -53,23 +62,32 @@ class GameState:
                 left, middle, right = pygame.mouse.get_pressed()
                 if not left:
                     for o in self.object_handles:
-                        o.fields["handle"].grabbed = False
+                        if "handle" in o.fields:
+                            o.fields["handle"].grabbed = False
 
         state.update_mouse_pos()
 
         screen.fill(black)
         for o in self.object_handles:
-            if o.fields["handle"].grabbed:
-                o_rect = o.fields["rect"]
-                o_handle = o.fields["handle"]
-                o_rect.x = self.mouse_pos.x - o_handle.grab_offset[0]
-                o_rect.y = self.mouse_pos.y - o_handle.grab_offset[1]
+            if "handle" in o.fields:
+                if o.fields["handle"].grabbed:
+                    if "handle" in o.fields and "rect" in o.fields:
+                        o_rect = o.fields["rect"]
+                        o_handle = o.fields["handle"]
+                        o_rect.x = self.mouse_pos.x - o_handle.grab_offset[0]
+                        o_rect.y = self.mouse_pos.y - o_handle.grab_offset[1]
             
             screen.blit(o.fields["object"], o.fields["rect"])
         pygame.display.flip()
 
 state = GameState()
-state.add_object(pygame.image.load("intro_ball.gif"))
-state.add_object(pygame.transform.scale(pygame.image.load("card_king_hearts.jpg"), (200, 300)))
+enemy_board = state.add_object(pygame.transform.scale(pygame.image.load("playing_board.jpg"), (400, 200)))
+state.add_object(pygame.image.load("intro_ball.gif"), draggable=True)
+state.add_object(pygame.transform.scale(pygame.image.load("card_king_hearts.jpg"), (100, 200)), draggable=True)
+state.add_object(pygame.transform.scale(pygame.image.load("ranger_playing_board.jpg"), (200, 100)), draggable=True)
+
+state.object_handles[enemy_board].fields["rect"].x = 300
+state.object_handles[enemy_board].fields["rect"].y = 300
+
 while 1:
     state.step()
